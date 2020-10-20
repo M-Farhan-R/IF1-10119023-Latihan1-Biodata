18acf2a8c257373809f8eab468742f55278f733f
Farhan <farhan.10119023@mahasiswa.unikom.ac.id>
2020-10-20 19:07:28 +0700

    Latihan 1 - Biodata

---
+++ src/if1/pkg10119023/latihan1/biodata/IF110119023Latihan1Biodata.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10119023.latihan1.biodata;
+
+/**
+ *
+ * @author 
+ * NAMA     : Muhammad Farhan R
+ * KELAS    : IF1
+ * NIM      : 10119023
+ * Deskripsi Program : Program ini berisi program untuk menampilkan jenis-jenis
+ * tipe data bilangan bulat
+ * 
+ */
+public class IF110119023Latihan1Biodata {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("BIODATA ROCKSTAR" + "\n" + "---------------------");
+        System.out.println("NAMA\t\t: Muhammad Farhan R");
+        System.out.println("ALAMAT\t\t: Bandung");
+        System.out.println("TEMPAT LAHIR\t: Bandung");
+        System.out.println("TANGGAL LAHIR\t: 18 Juli 2001");
+        System.out.println("---------------");
+        System.out.println("HOBI\t\t: Main");
+        System.out.println("MAKES\t\t: Makanan");
+        System.out.println("MIKES\t\t: Minuman");
+    }
+    
+}
